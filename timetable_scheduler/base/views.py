00e38fd36383a923f433ca2547d490273b5dda60
tomecki <tomek.dziopa@gmail.com>
2013-11-23 15:59:39 +0100

    Bootstrap

--- timetable_scheduler/base/views.py
+++ timetable_scheduler/base/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, render_to_response
-
+from django.template import RequestContext
 # Create your views here.
 
 def home(request):
     """ Home page"""
-    return render_to_response('home.html', {})
+
+    return render_to_response('home.html', {}, context_instance=RequestContext(request))
