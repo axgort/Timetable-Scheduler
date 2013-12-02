3a5ce64fb6d0d1d86b3596b664d1c4149a5abe33
havk <jastrzab5@gmail.com>
2013-12-02 15:37:55 +0100

    Celery dziala z Django

--- timetable_scheduler/scheduler/views.py
+++ timetable_scheduler/scheduler/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, render_to_response
-from scheduler.forms import DataForm
 from django.core.context_processors import csrf
 from django.template import RequestContext
+from scheduler.forms import DataForm
+from timetable_scheduler.celery import debug_havk
 
 # Create your views here.
 
@@ -12,14 +13,14 @@ def handle_uploaded_file(f):
             destination.write(chunk)
 
 def scheduler(request):
-
     if request.method == 'POST':
         form = DataForm(request.POST, request.FILES)
         print "POST"
         if form.is_valid():
             print "Valid"
             handle_uploaded_file(request.FILES['constraints'])
-            cd = form.cleaned_data
+            debug_havk.delay()
+            #cd = form.cleaned_data
             #send_mail(
                 #cd['subject'],
                 #cd['message'],
